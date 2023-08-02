9deedc35e8bbede9bff4ae58ac8c4f267ac42155
502 <502@DESKTOP-2FP96HR>
2023-08-02 11:58:20 +0900

    공지사항 검색기능 빠진 내용 다시 커밋

--- javachip/src/main/java/com/javachip/controller/HelpController.java
+++ javachip/src/main/java/com/javachip/controller/HelpController.java
@@ -1,7 +1,10 @@
 package com.javachip.controller;
 
+<<<<<<< HEAD
+=======
 import java.util.List;
 
+>>>>>>> branch 'main' of https://github.com/reachless12/javaChip.git
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
@@ -13,13 +16,17 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.javachip.service.HelpService;
 import com.javachip.vo.NoticeVO;
+
 import com.javachip.vo.SearchVO;
 import com.javachip.vo.UserVO;
+
 
 @Controller
 @RequestMapping(value="/help")
 public class HelpController {
 	
+	@Autowired
+	private HelpService helpService;
 	@Autowired HelpService helpService;
 	
 	// faq
@@ -94,6 +101,33 @@ public class HelpController {
 	}
 	
 	@RequestMapping(value="/noticeWrite.do", method = RequestMethod.POST)
+	
+	public String noticeWriteAction(NoticeVO noticeVO,HttpServletRequest req) {
+		
+		// String으로 값을 받음.
+		HttpSession session = req.getSession();
+		
+		// login한 계정의 type이 관리자인지 확인
+		// !! - 추가 - !!
+		
+		
+		// 공지사항 글쓰기
+		// !!-- 추후 세션에서 uNO를 로그인 값 받아옴. --!!
+		
+		//UserVO loginVO = (UserVO)session.getAttribute("login");
+		
+		// if(loginVO == null || 관리자가 아닐때) {
+		// return "redirect:notice.do";
+		// }
+		
+		int uNo = 1;
+		
+		noticeVO.setuNo(uNo);
+		
+		int result = helpService.insertNotice(noticeVO);
+		
+		System.out.println(noticeVO.toString());
+
 	public String noticeWriteAction(NoticeVO noticeVO, HttpServletRequest req) {
 		
 		HttpSession session = req.getSession();
