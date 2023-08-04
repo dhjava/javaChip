c880af4a67285559b2a95c3e9d9c8d5ae22cd92a
502-2 <502-2@DESKTOP-807KB4E>
2023-08-04 12:11:50 +0900

    장바구니부터 결제까지의 기능 백단 임시 완성

--- javachip/src/main/java/com/javachip/controller/ShopController.java
+++ javachip/src/main/java/com/javachip/controller/ShopController.java
@@ -1,11 +1,27 @@
 package com.javachip.controller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.javachip.service.CartService;
+import com.javachip.vo.CartVO;
+import com.javachip.vo.UserVO;
 
 @Controller
 @RequestMapping(value="/shop")
 public class ShopController {
+	
+	@Autowired
+	private CartService cs;
 	
 	@RequestMapping(value="/grid.do")
 	public String grid() {
@@ -17,7 +33,17 @@ public class ShopController {
 		return "shop/details";
 	}
 	
-	@RequestMapping(value="/checkout.do")
+	@RequestMapping(value="/checkout.do", method=RequestMethod.GET)
+	public String checkout(HttpServletRequest req, Model model
+			) {
+		HttpSession session = req.getSession();
+		UserVO loginVO = (UserVO)session.getAttribute("login");
+		if(loginVO==null) {
+			return "redirect:/member/login.do";
+		}
+		return "shop/checkout";
+	}
+	@RequestMapping(value="/checkout.do", method=RequestMethod.POST)
 	public String checkout() {
 		return "shop/checkout";
 	}
