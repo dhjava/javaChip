0e5522cf54df1896d58335f02e8603850194767a
502-2 <502-2@DESKTOP-807KB4E>
2023-09-05 09:26:59 +0900

    정기배송 페이지 오류 해결 및 헤더 장바구니 개수 알림 기능 활성화

--- javachip/src/main/java/com/javachip/controller/HomeController.java
+++ javachip/src/main/java/com/javachip/controller/HomeController.java
@@ -2,25 +2,41 @@ package com.javachip.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.javachip.service.CartService;
 import com.javachip.service.ProductService;
 import com.javachip.vo.ProductVO;
 import com.javachip.vo.SearchVO;
+import com.javachip.vo.UserVO;
 
 @Controller
 public class HomeController {
 	
 	@Autowired
 	private ProductService ps;
+	@Autowired
+	private CartService cs;
 	
 	// 메인페이지
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Model model) {
+	public String home(
+			HttpServletRequest req
+		,	Model model
+			) {
+		HttpSession session = req.getSession();
+		UserVO loginVO = (UserVO)session.getAttribute("login");
+		if(loginVO!=null) {
+			int cntCart = cs.countUserCart(loginVO.getuNo());
+			session.setAttribute("cntCart", cntCart);
+		}
 		// 인기순 정렬(별도 쿼리)
 		List<ProductVO> indexList = ps.selectIndexProduct();
 		
